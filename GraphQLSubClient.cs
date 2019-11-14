f31305a8d0ec324912a71079d40fb0d66fcc5c37
Frazier <bfrazier@tx-ltmac14984.corp.epsilon.com>
2019-11-13 19:55:59 -0600

    Initial commit

---
+++ GraphQLSubClient.cs
@@ -0,0 +1,32 @@
+using System;
+using System.Net.Mqtt;
+using System.Net.Http;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using System.Text;
+
+namespace GraphQLSubClient
+{
+    public class GraphQLSubClient {
+
+        private string apiUrl;
+        private HttpClient httpClient;
+
+        public GraphQLSubClient(string _apiUrl, Dictionary<string, string> headers) {
+            apiUrl = _apiUrl;
+            httpClient = new HttpClient();
+            foreach (KeyValuePair<string, string> header in headers)
+            {
+                httpClient.DefaultRequestHeaders.Add(header.Key, header.Value);
+            }
+        }
+
+        public async void AddSubscription(string queryString, Action callbackMethod) {
+            HttpContent content = new StringContent("{\"query\":" + queryString, Encoding.UTF8, "application/json");
+            HttpResponseMessage response = await httpClient.PostAsync(apiUrl, content);
+            
+        }
+
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using GraphQLSubClient;
+
+namespace graphql_sub_client
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
